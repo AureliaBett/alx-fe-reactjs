5d432fa3e0453d3c188296d9e9412618ecf932c4
AureliaBett <cbettaurelia@gmail.com>
2025-11-08 00:47:54 +0300

    Apply Inline Css Components to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: "10px", textDecoration: "underLine"}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
