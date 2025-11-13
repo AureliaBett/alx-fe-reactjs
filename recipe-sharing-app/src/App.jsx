17f04088906cfb3bd68ef75a22f2775b20a092be
AureliaBett <cbettaurelia@gmail.com>
2025-11-13 06:12:56 +0300

    Refresh

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  )
+}
+
+export default App
+
