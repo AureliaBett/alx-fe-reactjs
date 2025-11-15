643b65fa996a5e3df4ed36d599fd83ec8ce531f9
AureliaBett <cbettaurelia@gmail.com>
2025-11-15 17:02:23 +0300

    Updated and deletd Commands

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,10 @@ import {create} from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (deletedRecipe) => set(state => ({ recipes: [...state.recipes, deleteRecipe] })),
+  updateRecipe: (deletedRecipe) => set(state => ({ recipes: [...state.recipes, deleteRecipe] })),
+
 }));
 
 export default useRecipeStore
