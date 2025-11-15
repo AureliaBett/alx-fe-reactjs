1d9abf3ce992020210d4e5f72c0225c400b0d227
AureliaBett <cbettaurelia@gmail.com>
2025-11-15 17:15:30 +0300

    Routes in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,30 @@
+// File: src/App.jsx
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
 import DeleteRecipeButton from './components/DeleteRecipeButton'
-
 import './App.css'
 
 function App() {
- 
+  const [description, setDescription] = useState('');
 
   return (
-    <div>
-      <div style={{ backgroundColor: "lightblue", padding: "50px"}}> <AddRecipeForm /></div>
-      
-      
-      <div style={{ backgroundColor: "blue", padding: "50px"}} ><RecipeList /></div>
-      
-    </div>
-    
-
-
+    <Router>
+      <div>
+        <div style={{ backgroundColor: "lightblue", padding: "50px" }}>
+          <AddRecipeForm />
+        </div>
+        <div style={{ backgroundColor: "blue", padding: "50px" }}>
+          <Routes>
+            <Route path="/" element={<RecipeList />} />
+            <Route path="/recipes/:id" element={<RecipeDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
 export default App
-
