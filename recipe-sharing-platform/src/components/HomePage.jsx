d588cedb2957dce307a50f6dceda4c969aa72223
AureliaBett <cbettaurelia@gmail.com>
2025-12-03 22:54:35 +0300

    add styling to the Homepage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,19 +10,19 @@ function HomePage() {
 );
 
   return (
-    <div>
+    <div className="recipeCard bg-gray-1000 sm:p-4 md:p-8 sm: text-lg md:text-xl max-w-sm max-auto my-20 rounded-lg shadow-lg">
         <div>
             <h1> Recipe Sharing App</h1>
             <p>Cook to your hearts desires with our Recipes</p>
             
         </div>
-        <div>
+        <div className ="bg-red-100 rounded-lg" >
           {recipes.map((recipe) =>(
           <div key ={recipe.id} >
-            <img src={recipe.img}  ></img>
-            <div>
-              <h2>{recipe.title}</h2>
-              <p>{recipe.summary}</p>
+            <img src={recipe.image} className = "sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"  ></img>
+            <div  className="text-blue-800" >
+              <h2 className="my-4">{recipe.title}</h2>
+              <p className="text-base">{recipe.summary}</p>
             </div>
           </div>
             
