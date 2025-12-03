3fcfb9eb267824b7f55ac863f531e6ec26f84d7e
AureliaBett <cbettaurelia@gmail.com>
2025-12-03 22:03:01 +0300

    Setting Up HomePage Component Using useState and useEffects

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -2,20 +2,34 @@ import React, {useState, useEffect} from "react";
 import data from "../data.json"
 
 function HomePage() {
-  const [recipe, setRecipe] = useState([]);
+  const [recipes, setRecipe] = useState([]);
   useEffect(() =>{
     setRecipe(data)
 
-  }
-)
+  }, []
+);
 
   return (
     <div>
         <div>
             <h1> Recipe Sharing App</h1>
             <p>Cook to your hearts desires with our Recipes</p>
+            
+        </div>
+        <div>
+          {recipes.map((recipe) =>(
+          <div key ={recipe.id} >
+            <img src={recipe.img}  ></img>
+            <div>
+              <h2>{recipe.title}</h2>
+              <p>{recipe.summary}</p>
+            </div>
+          </div>
+            
+          ))}
         </div>
 
     </div>
   );
 }
+export default HomePage
