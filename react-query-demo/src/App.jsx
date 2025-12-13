585fd3bc399b9ca407cdfd02fff24dceb3b102d2
AureliaBett <cbettaurelia@gmail.com>
2025-12-13 04:07:04 +0300

    React Query setup in main component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
-
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+    );
+  }
 export default App
